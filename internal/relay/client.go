789a0fd4c1db1dc16c7b29f6c87680da12e3cd5b
M Indraneeli Vardhan <vardhanm0257@gmail.com>
2026-02-15 18:30:39 +0530

    Validate MEV relay JSON schema with lossless decoding

--- internal/relay/client.go
+++ internal/relay/client.go
@@ -2,12 +2,24 @@ package relay
 
 import (
 	"fmt"
+	"encoding/json"
 	"io"
 	"net/http"
 	"os"
 	"time"
 )
 
+type RelayBid struct {
+	Slot      string `json:"slot"`
+	ValueWei string `json:"value"`
+}
+
+/*************  ✨ Windsurf Command ⭐  *************/
+// FetchAndStore retrieves the current relay bid traces from the given
+// baseURL and stores the result in a JSON file in the given outDir.
+// The filename of the stored file will be in the format
+// `<baseURL>_<unix_timestamp>.json`.
+/*******  9f6fcb3f-4c66-423b-b7b0-1c65513ec1da  *******/
 func FetchAndStore(baseURL, outDir string) error {
 	endpoint := fmt.Sprintf(
 		"%s/relay/v1/data/bidtraces/proposer_payload_delivered",
@@ -23,6 +35,10 @@ func FetchAndStore(baseURL, outDir string) error {
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
+		return err
+	}
+	var bids []RelayBid
+	if err := json.Unmarshal(body, &bids); err != nil {
 		return err
 	}
 
